2cc65d12af70ea7feb8b863c764f5674211b6098
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-08-22 19:21:43 -0500

    all input into single output obj

--- assessment/index.js
+++ assessment/index.js
@@ -7,10 +7,10 @@ const inputB =
 const inputC =
   "Smith | Steve | D | M | Red | 3-3-1985 Bonk | Radek | S | M | Green | 6-3-1975 Bouillon | Francis | G | M | Blue | 6-3-1975";
 
-  
 // function to remove the delimiters
 function removeDelimiter(string) {
-  return string.replace(/[,\|]/g, "");
+  // return string.replace(/[,\|]/g, "");
+  return string.replace(/[\|][ ]|,/g, "");
 }
 
 // transform raw data into individual entries "people/person"
@@ -22,7 +22,7 @@ let person3 = groupA.slice(12);
 
 // Run inputB through the removeDelimiter function to get it into the same format as inputA
 
-let noCommas = removeDelimiter(inputB)
+let noCommas = removeDelimiter(inputB);
 
 let groupB = noCommas.split(" ");
 // split the groupB into individual persons
@@ -30,7 +30,15 @@ let person4 = groupB.slice(0, 5);
 let person5 = groupB.slice(5, 10);
 let person6 = groupB.slice(10);
 
-// combine all all persons into one People obj
+let noBars = removeDelimiter(inputC);
+
+let groupC = noBars.split(" ");
+// split the groupC into individual persons
+let person7 = groupC.slice(0, 6);
+let person8 = groupC.slice(6, 12);
+let person9 = groupC.slice(12);
+
+// combine all all persons into one People object
 let people = [
   {
     firstName: person1[1],
@@ -61,7 +69,7 @@ let people = [
     lastName: person4[0],
     middleInitial: null,
     dob: person4[4],
-    gender: person4[2],
+    gender: person4[2][0],
     color: person4[3],
   },
   {
@@ -69,7 +77,7 @@ let people = [
     lastName: person5[0],
     middleInitial: null,
     dob: person5[4],
-    gender: person5[2],
+    gender: person5[2][0],
     color: person5[3],
   },
   {
@@ -77,15 +85,36 @@ let people = [
     lastName: person6[0],
     middleInitial: null,
     dob: person6[4],
-    gender: person6[2],
+    gender: person6[2][0],
     color: person6[3],
+  },
+  {
+    firstName: person7[1],
+    lastName: person7[0],
+    middleInitial: person7[2],
+    dob: person7[5],
+    gender: person7[3],
+    color: person7[4],
+  },
+  {
+    firstName: person8[1],
+    lastName: person8[0],
+    middleInitial: person8[2],
+    dob: person8[5],
+    gender: person8[3],
+    color: person8[4],
+  },
+  {
+    firstName: person9[1],
+    lastName: person9[0],
+    middleInitial: person9[2],
+    dob: person9[5],
+    gender: person9[3],
+    color: person9[4],
   },
 ];
 
-// console.log(people);
-
-
-
+console.log(people);
 
 // user = [{
 //   firstName: "name",

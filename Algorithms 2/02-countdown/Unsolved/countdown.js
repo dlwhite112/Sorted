2225879706f9ba80e8ab82244c58da8bb7ea6629
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-09-13 22:06:50 -0500

    solved countdown, passing tests

--- Algorithms 2/02-countdown/Unsolved/countdown.js
+++ Algorithms 2/02-countdown/Unsolved/countdown.js
@@ -1,4 +1,13 @@
 // Write code to print all numbers from `num` down to 1
 // You may assume `num` will be a positive number
 
-var countdown = function(num) {};
+var countdown = function(num) {
+    let arr = []
+    for (let i = 1;num > 0; num--){
+        i-=num
+        console.log(num)
+    }
+    
+};
+
+countdown(10)
